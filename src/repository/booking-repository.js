32efdcc8d6ea86116a9edb93351a2217cc8c1daa
saheb <sahebraoj24pc@gmail.com>
2024-09-28 11:47:14 +0530

    Added migration to add new coloums to booking table

--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -7,12 +7,26 @@ const { AppError,ValidationError } = require('../utils/errors');
 class BookingRepository{
     async create(data){
         try{
+            const booking=await Booking.create(data);
 
         }catch(error){
             if(error.name==='SequelizeValidationError'){
                 throw new ValidationError(error);
             }
-            throw new AppError();
+            throw new AppError(
+                'ReposistoryError',
+                'Cannot create Booking',
+                'There was some issue creating the booking,please tgyr again later',
+                StatusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    async update(data){
+        try{
+
+        }catch(error){
+            
         }
     }
 }
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,9 @@
 const express=require('express');
 
-const v1ApiRoutes=require('./v1/index');
+//const v1ApiRoutes=require('../index')
 
 const router=express.Router();
 
-router.use('/v1',v1ApiRoutes);
+//router.use('/v1',v1ApiRoutes);
 
 module.exports=router
